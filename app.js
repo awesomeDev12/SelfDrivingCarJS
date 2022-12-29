6061cb8d7ce554b899ab1755d1a5ec7906667765
Gardevoir <you@example.com>
2022-12-29 19:31:29 +0530

    Car moves on key down

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const canvas = document.querySelector('#myCanvas');
-canvas.height = window.innerHeight;
 canvas.width = 200;
 
 const car=new Car(100,100,30,50);
 const ctx=canvas.getContext("2d");
 car.draw(ctx);
+
+
+animate();
+
+function animate(){
+    car.update();
+    canvas.height = window.innerHeight;
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
