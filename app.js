a020e16d34ed2348e8ed86b7aa491f6cc7b7e35f
Gardevoir <you@example.com>
2022-12-29 18:27:03 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const canvas = document.querySelector('#myCanvas');
+canvas.height = window.innerHeight;
+canvas.width = 200;
+
+const car=new Car(100,100,30,50);
+cons ctx=canvas.getContext("2d");
+car.draw(ctx);
