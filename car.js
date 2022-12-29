8fe7bdefc8759749a841cf004b9e4e561df57341
Gardevoir <you@example.com>
2022-12-29 20:26:49 +0530

    Friction, Left/Right movement

--- car.js
+++ car.js
@@ -10,6 +10,7 @@ class Car{
         this.acceleration=0.2;
         this.maxSpeed=3;
         this.maxReverseSpeed=-2;
+        this.friction=0.1;
     }
 
     update(){
@@ -26,9 +27,35 @@ class Car{
         if(this.speed<this.maxReverseSpeed){
             this.speed=this.maxReverseSpeed;
         }
-
+        
         this.y-=this.speed;
+
+        //friction
+        //kinetic friction
+        if(this.speed>0){
+            this.speed-=this.friction;
+        }
+        if(this.speed<0){
+            this.speed+=this.friction;
+        }
+        //static friction
+        //if we do not write the following code 
+        //then then upon releasing in just the right way
+        //the car will always be in motiion
+        //this is because if the speed is not exactly zero then 
+        //then friction will move it up and down continuously for eternity
+        if(Math.abs(this.speed)<this.friction){
+            this.speed=0;
+        }
+
+        if(this.controls.left){
+            this.x-=2;
+        }
+        if(this.controls.right){
+            this.x+=2;
+        }
     }
+
     draw(ctx){
         ctx.beginPath();
         ctx.rect(
